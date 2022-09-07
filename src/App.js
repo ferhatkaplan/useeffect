d2db24c481276b68e63d6c5b19c384a4113f0405
ferhat <ferhatkaplan2742@gmail.com>
2022-09-08 01:41:33 +0300

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import react, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const[count, setcount]= useState(0)
+  const[button, setbutton]= useState('User')
+  console.log("Sayfa Açıldı, burası Global değer")
+
+  useEffect(()=>{
+    console.log(`Basılan button ${button} `)
+  },[button])
+
+  useEffect(()=>{
+    console.log(`Basılan button ${count} `)
+  },[count])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={()=>setbutton("User")}>User</button>
+        <button onClick={()=>setbutton("Comments")}>Comments</button>
+        <button onClick={()=>setbutton("Posts")}>Posts</button>
+        <h1>{button}</h1>
+      </div>
+      <button onClick={()=>setbutton("Count")}>Count</button>
+      <h1></h1>
+
     </div>
   );
 }
